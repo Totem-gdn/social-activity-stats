dfc1d4247db0e800dea10cc83cf56a8f40e8b583
uwulily <lz@girlc.at>
2022-06-28 12:13:11 +0200

    changes as requested

--- main.py
+++ main.py
@@ -26,10 +26,16 @@ bot = hikari.GatewayBot(
         force_color=True
 )
 
-@bot.listen(hikari.MessageCreateEvent)
-async def on_message_create(event: hikari.MessageCreateEvent):
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def on_message_create(event: hikari.GuildMessageCreateEvent):
 	logging.info(f"New message {event.channel_id}/{event.message_id} by {event.author_id}.")
-#	logging.debug(f"event:{event} {dir(event)}")
+	#logging.debug(f"event:{event} {dir(event)}")
+
+	cha = bot.cache.get_guild_channel(event.channel_id)
+	if cha is None:
+		logging.info(f"{event.channel_id} not cached, fetching")
+		cha = await bot.rest.fetch_channel(event.channel_id)
+		await bot._cache.set_guild_channel(cha)
 
 	# If messages is a reply set reff to the id of the message its replying to | else set it to None
 	ref = event.message.referenced_message
@@ -37,51 +43,51 @@ async def on_message_create(event: hikari.MessageCreateEvent):
 		reff = ref.id
 	else:
 		reff = None
+
 	data = {
-		"user_id": event.author_id,
-		"username": event.author.username,
-		"user_discriminator": event.author.discriminator,
+		"user_id": event.member.id,
+		"username": event.member.username,
+		"user_discriminator": event.member.discriminator,
+		"user_displayname": event.member.display_name,
 		"channel_id": event.channel_id,
+		"channel_name":cha.name,
 		"guild_id": event.message.guild_id,
 		"timestamp": event.message.timestamp,
-		"text": event.message.content[:30],  # limit size of text sent 
-		"text_len": len(event.message.content),
-		"attachments": [i.url for i in event.message.attachments], 
+		"text": event.message.content[:30] if isinstance(event.message.content, str) else "",
+		"text_len": len(event.message.content) if isinstance(event.message.content, str) else 0,
+		"attachments": [i.url for i in event.message.attachments],
 		"in_reply_to": reff
 	}
-
 	# add the event to the flush queue
 	# we use event.__class__.__name__ to get the name of the event
 	mp_client.track(event.author_id, event.__class__.__name__, data)
 
 
-@bot.listen(hikari.ReactionAddEvent)
-async def on_reaction_add(event: hikari.ReactionAddEvent):
+@bot.listen(hikari.GuildReactionAddEvent)
+async def on_reaction_add(event: hikari.GuildReactionAddEvent):
 	logging.info(f"New reaction to {event.channel_id}/{event.message_id} by {event.user_id}: {event.emoji_name}.")
 	
 	# Getting the guild id of the channel
-	cha = await bot.rest.fetch_channel(event.channel_id)
-	try:
-		# Try getting it
-		gid = cha.guild_id
-	except:
-		# Else set it to None
-		gid = None
-	
+	cha = bot.cache.get_guild_channel(event.channel_id)
+	if cha is None:
+		logging.info(f"{event.channel_id} not cached, fetching")
+		cha = await bot.rest.fetch_channel(event.channel_id)
+		await bot._cache.set_guild_channel(cha)	
+
 	# if emoji is a custom emoji set its name to emoji_id:emoji_name | else set it to the unicode emoji
 	if event.emoji_id:
 		emoji_name = f"{event.emoji_id}:{event.emoji_name}"
 	else:
 		emoji_name = event.emoji_name
 
-	# Fetching the user so we can get their username and the discriminator
-	user = await bot.rest.fetch_user(event.user_id)
 	data = {
 		"user_id": event.user_id,
-		"username": user.username,
-		"user_discriminator": user.discriminator,
+		"username": event.member.username,
+		"user_discriminator": event.member.discriminator,
+		"user_displayname": event.member.display_name,
 		"channel_id": event.channel_id,
-		"guild_id": gid,
+		"channel_name": cha.name,
+		"guild_id": event.guild_id,
 		"timestamp": int(time.time()),
 		"to_message_id": event.message_id,
 		"emoji_name": emoji_name,
