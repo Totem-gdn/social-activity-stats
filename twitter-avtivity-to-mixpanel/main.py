2629420f966a1b9f07b79da43d97f4806b429a68
AlexTyutyunnykUA <alext19999@gmail.com>
2022-11-08 11:17:24 +0200

    changed func get new tweets.

--- twitter-avtivity-to-mixpanel/main.py
+++ twitter-avtivity-to-mixpanel/main.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 import threading
@@ -35,12 +36,13 @@ auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
 auth.set_access_token(config["access_token"], config["access_token_secret"])
 # Initialization Twitter API
 api = tweepy.API(auth, wait_on_rate_limit=True)
-#client = tweepy.Client(bearer_token=config["bearer_token"], consumer_key=config["consumer_key"],
-#                       consumer_secret=config["consumer_secret"], access_token=config["access_token"],
-#                       access_token_secret=config["access_token_secret"])
+client = tweepy.Client(bearer_token=config["bearer_token"], consumer_key=config["consumer_key"],
+                       consumer_secret=config["consumer_secret"], access_token=config["access_token"],
+                       access_token_secret=config["access_token_secret"])
 
 
 def send_profile_to_mixpanel(user):
+    # Set properties for Mixpanel Profile
     properties = {
         'followersCount': user['followers_count'],
         'FriendsCount': user['friends_count'],
@@ -54,97 +56,84 @@ def send_profile_to_mixpanel(user):
     if user['location'] != '':
         properties['location'] = user['location']
 
+    # Send to MixPanel Profile
     mp_client.people_set(user['id_str'], properties)
 
 
 def update_followers_data():
     count = 0
-
     for follower in Cursor(api.get_followers).items():
         count += 1
         follower_json = json.dumps(follower._json)
         follower_data = json.loads(follower_json)
         print(count, " ", follower_data, '\n')
-
         send_profile_to_mixpanel(follower_data)
         time.sleep(4)
 
 
-threading.Thread(target=update_followers_data).start()
+def send_tweet_to_mixpanel(id):
+    status = api.get_status(id)
+    # Convert tweepy.models.Status to JSON
+    data_json = json.dumps(status._json)
+    # Convert JSON to dict
+    tweet_data = json.loads(data_json)
+    print(tweet_data)
+    # Get params from tweet data
+    user = tweet_data['user']
+    hashtags = tweet_data['entities']['hashtags']
+    distinct_id = user['screen_name']
+    url = "https://twitter.com/user/status/{}".format(id)
+
+    # Set properties for Mixpanel
+    properties = {
+        'createdAt': user['created_at'],
+        'followersCount': user['followers_count'],
+        'ID': tweet_data['id'],
+        'lang': tweet_data['lang'],
+        'retweetCount': tweet_data['retweet_count'],
+        'text': tweet_data['text'],
+        'retweeted': 'False',
+        'url': url
+    }
+
+    if len(hashtags) != 0:
+        hashtag_str = ''
+        for hashtag in hashtags:
+            hashtag_str += ''.join(hashtag['text'] + ',')
+
+        properties['hashtag'] = hashtag_str.rstrip(',')
+
+    if 'retweeted_status' in tweet_data:
+        retweetedUserName = tweet_data['entities']['user_mentions'][0]['screen_name']
+
+        properties['retweeted'] = 'True'
+        properties['retweetedUserName'] = retweetedUserName
+
+    if 'media' in tweet_data['entities']:
+        properties['mediaUrl'] = tweet_data['entities']['media'][0]['media_url']
+
+    # Send to Mixpanel
+    mp_client.track(distinct_id, 'TestEventNewTweet', properties)
+    logger.info('New tweet: ', tweet_data['text'])
 
 
-class MyStream(tweepy.StreamingClient):
-    # This function gets called when the stream is working
-    def on_connect(self):
-        logger.info("Connected")
-
-    # This function gets called when published new tweet
-    def on_tweetl(self, tweet):
-        # Get all information about tweet
-        status = api.get_status(tweet.id)
-        # Convert tweepy.models.Status to JSON
-        data_json = json.dumps(status._json)
-        # Convert JSON to dict
-        tweet_data = json.loads(data_json)
-
-        # Get params from tweet data
-        user = tweet_data['user']
-        hashtags = tweet_data['entities']['hashtags']
-        distinct_id = user['screen_name']
-        url = "https://twitter.com/user/status/{}".format(tweet.id)
-
-        # Set properties for Mixpanel
-        properties = {
-            'createdAt': user['created_at'],
-            'followersCount': user['followers_count'],
-            'ID': tweet_data['id'],
-            'lang': tweet_data['lang'],
-            'retweetCount': tweet_data['retweet_count'],
-            'text': tweet_data['text'],
-            'retweeted': 'False',
-            'url': url
-        }
-
-        if len(hashtags) != 0:
-            hashtag_str = ''
-            for hashtag in hashtags:
-                hashtag_str += ''.join(hashtag['text'] + ',')
-
-            properties['hashtag'] = hashtag_str.rstrip(',')
-
-        if 'retweeted_status' in tweet_data:
-            retweetedUserName = tweet_data['entities']['user_mentions'][0]['screen_name']
-
-            properties['retweeted'] = 'True'
-            properties['retweetedUserName'] = retweetedUserName
-
-        if 'media' in tweet_data['entities']:
-            properties['mediaUrl'] = tweet_data['entities']['media'][0]['media_url']
-
-        # Send to Mixpanel
-        mp_client.track(distinct_id, 'TestEventNewTweet', properties)
-        logger.info('New tweet: ', tweet.text)
-
-    def on_disconnect(self):
-        logger.info("Disconected")
-        self.filter()
-        logger.info("Reconecting...")
+def get_new_tweets():
+    while True:
+        now = datetime.datetime.now()
+        last_five_minutes = now - datetime.timedelta(minutes=5)
+        tweets = client.get_users_tweets(config['user_id'], start_time=last_five_minutes)
+        tweet = tweets[0]
+        if tweet is not None:
+            for i in tweet:
+                print(i['id'])
+                send_tweet_to_mixpanel(i['id'])
+        else:
+            logger.info('Dosen`t have new tweets last 5 minutes.')
+            pass
+        time.sleep(300)
 
 
-# Create stream
-stream = MyStream(bearer_token=config["bearer_token"], wait_on_rate_limit=True)
+if __name__ == "__main__":
+    threading.Thread(target=get_new_tweets).start()
+    update_followers_data()
 
-# Set rules
-# query = 'from:{}'.format(config['user_id'])
-query = 'from:1466050209847427075'
-stream.add_rules(add=tweepy.StreamRule(query))
-
-# Delete rules
-# stream.delete_rules('')
-
-# Get rules
-rules = stream.get_rules()
-logger.info("Rules: ", rules)
-
-# Start server
-stream.filter()
